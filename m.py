c499980bac9fc240a32d916c28e59b8a455ddf26
kasra shiralizadeh <135124713+kasra1389@users.noreply.github.com>
2025-11-02 01:41:25 +0330

    Add files via upload
    ماشین حساب بر پایه ترمینال با پایتون

---
+++ m.py
@@ -0,0 +1,64 @@
+import random
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def calculate():
+    operation = input('''
+Please type in the math operation you would like to complete:
++ for addition
+- for subtraction
+* for multiplication
+/ for division
+
+Answer ==>:  ''')
+
+    number_1 = float(input('Please enter the first number==>: '))
+    number_2 = float(input('Please enter the second number==>: '))
+
+    if operation == '+':
+        print('{} + {} = '.format(number_1, number_2))
+        print(number_1 + number_2)
+
+    elif operation == '-':
+        print('{} - {} = '.format(number_1, number_2))
+        print(number_1 - number_2)
+
+    elif operation == '*':
+        print('{} * {} = '.format(number_1, number_2))
+        print(number_1 * number_2)
+
+    elif operation == '/':
+        print('{} / {} = '.format(number_1, number_2))
+        print(number_1 / number_2)
+
+    else:
+        print('You have not typed a valid operator, please run the program again ')
+
+    # Add again() function to calculate() function
+    again()
+
+
+def again():
+    calc_again = input('''
+Do you want to calculate again?
+Please type Y for YES or N for NO.
+''')
+
+    if calc_again.upper() == 'Y':
+        calculate()
+    elif calc_again.upper() == 'N':
+        print('See you later.')
+    else:
+        again()
+
+
+calculate()
+
+
+def kasra():
+    print('finish and welcome to kasra A B Z A R')
+
+
+kasra()
